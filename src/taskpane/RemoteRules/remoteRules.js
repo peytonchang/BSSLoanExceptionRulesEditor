12695f9dfbbe6d4cddca3ef66d06ac1ff9d9bf93
peytonchang <peytonchang07@gmail.com>
2024-06-27 10:51:57 -0400

    Remote Rules 31

--- src/taskpane/RemoteRules/remoteRules.js
+++ src/taskpane/RemoteRules/remoteRules.js
@@ -197,7 +197,7 @@
                 console.log("made it here (getLoanInputData) 6");
                 // Load necessary cells
                 const environmentCell = sheet.getCell(activeRow, dicColumn['Environment'] + 1);
-                console.log("environmentCell: " + environmentCell);
+                console.log("environmentCell: " + environmentCell[0]);
                 const ruleProjectCell = sheet.getCell(activeRow, dicColumn['Rule Project'] + 1);
                 console.log("ruleProjectCell: " + ruleProjectCell);
                 const loanNumberCell = sheet.getCell(activeRow, dicColumn['Loan #'] + 1);
