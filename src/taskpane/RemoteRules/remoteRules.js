e1c39f171f09f0624c5e67412ece456958e52e3e
peytonchang <peytonchang07@gmail.com>
2024-06-27 12:50:14 -0400

    Remote Rules 56

--- src/taskpane/RemoteRules/remoteRules.js
+++ src/taskpane/RemoteRules/remoteRules.js
@@ -154,7 +154,7 @@
                 let formattedResults = '';
     
                 console.log("made it here (executeRules) 13");
-                if (propertyExists(resultsJSON, 'result.inputData')) {
+                if (propertyExists(resultsJSON, 'result.serviceResults.businessRules')) {
                     console.log("made it here (executeRules) 14");
                     formattedResults = formatServiceResults(resultsJSON);
                 }
